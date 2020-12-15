25781bd315a4d7f081761eaa3a39b69e7d89a2f0
RaduMarius26 <72089021+RaduMarius26@users.noreply.github.com>
2020-12-15 12:18:50 +0200

    Update Users.java

--- coco/src/main/java/coco/aplication/Users.java
+++ coco/src/main/java/coco/aplication/Users.java
@@ -1,14 +1,12 @@
 package coco.aplication;
 
-import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+
 
 @Entity
-@Table(name = "Users")
 public class Users{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,16 +14,6 @@ public class Users{
 
     private String username, password, picture, followers;
 
-    public Users() {
-        username = "coco";
-        password = "coco";
-    }
-
-    public Users(Long userID, String username, String password) {
-        this.userID = userID;
-        this.username = username;
-        this.password = password;
-    }
 
     public Long getUserID() {
         return userID;
